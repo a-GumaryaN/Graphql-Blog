f03ce52415d7e381a8b51a797ed47ea9d69ba1c4
a-GumaryaN <alireza313gomarian@gmail.com>
2021-11-26 11:16:43 +0330

    delete dist from git history...

--- packages/server/dist/graphql/schemas/mutationSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Mutation = void 0;
-var graphql_1 = require("graphql");
-var PersonMutation_1 = require("./PersonMutation");
-exports.Mutation = new graphql_1.GraphQLObjectType({
-    name: "Mutation",
-    fields: { updatePerson: PersonMutation_1.updatePerson, register: PersonMutation_1.register }
-});
